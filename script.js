72ebffe3ff18d2479ffaaf2bf5351ee8c5aa77b1
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-02-18 11:54:31 +0000

    Navbar still shown when scrolling, updates to about me section, and more smaller changes

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
       icon.classList.toggle("open");
     });
   });
-  
 
+  window.addEventListener('scroll', function() {
+    const desktopNav = document.getElementById('desktop-nav');
+    const hamburgerNav = document.getElementById('hamburger-nav');
+  
+    if (window.scrollY > 50) { // Adjust scroll distance as needed
+      desktopNav.classList.add('shrink');
+      hamburgerNav.classList.add('shrink');
+    } else {
+      desktopNav.classList.remove('shrink');
+      hamburgerNav.classList.remove('shrink');
+    }
+  });
