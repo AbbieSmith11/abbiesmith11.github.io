cafe3da877d4848db385d221416f8e477d221528
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-02-24 16:49:20 +0000

    Created projects slider section

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const desktopNav = document.getElementById('desktop-nav');
     const hamburgerNav = document.getElementById('hamburger-nav');
   
-    if (window.scrollY > 50) { // Adjust scroll distance as needed
+    if (window.scrollY > 50) {
       desktopNav.classList.add('shrink');
       hamburgerNav.classList.add('shrink');
     } else {
